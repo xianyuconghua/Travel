6ad5875dd99d66482a579153acac8ae910612246
xianyuconghua <1064671075@qq.com>
2018-07-27 16:28:45 +0800

    8-1

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="header">
+		城市选择
+		<router-link to='/'>
+			<div class="iconfont header-back">&#xe624;</div>
+		</router-link>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'CityHeader'
+}
+</script>
+
+<style scoped>
+	.header{
+		overflow: hidden;
+		height: .86rem;
+		line-height: .86rem;
+		text-align: center;
+		color: #fff;
+		background: #00bcd4;
+		font-size: .32rem;
+		position: relative;
+	}
+	.header-back{
+		width: .64rem;
+		text-align: center;
+		position: absolute;
+		left: 0;
+		top: 0;
+		font-weight: 900;
+		color: #fff;
+	}
+</style>
